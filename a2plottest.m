76b37f0cc953847e0c63c95b0460a81367db51dd
Sarah Li <sarah_li_7@sfu.ca>
2023-04-11 22:40:45 -0700

    yaur

--- a2plottest.m
+++ a2plottest.m
@@ -16,21 +16,12 @@ lambda4 = 0.09;
 
 %initialize row vectors for storing 
 time = 0:0.1:t_end;
-f1t1 = zeros(1,length(time));
-f1t2 = zeros(1,length(time));
-f1t3 = zeros(1,length(time));
-
-f2t1 = zeros(1,length(time));
-f2t2 = zeros(1,length(time));
-f2t3 = zeros(1,length(time));
-
-f3t1 = zeros(1,length(time));
-f3t2 = zeros(1,length(time));
-f3t3 = zeros(1,length(time));
-
-f4t1 = zeros(1,length(time));
-f4t2 = zeros(1,length(time));
-f4t3 = zeros(1,length(time));
+%theta 1
+T1 = zeros(4,length(time));
+%theta 2
+T2 = zeros(4,length(time));
+%theta 3
+T3 = zeros(4,length(time));
 
 %evaluate position of the pendulum
 %lambda 1
@@ -43,9 +34,9 @@ for j = 1:length(time)
     %Theta3
     ft3 = f(t,t3,lambda1);
     %store into vector
-    f1t1(j) = ft1;
-    f1t2(j) = ft2;
-    f1t3(j) = ft3;
+    T1(1,j) = ft1;
+    T2(1,j) = ft2;
+    T3(1,j) = ft3;
 end
 %lambda 2
 for j = 1:length(time)
@@ -57,9 +48,9 @@ for j = 1:length(time)
     %Theta3
     ft3 = f(t,t3,lambda2);
     %store into vector
-    f2t1(j) = ft1;
-    f2t2(j) = ft2;
-    f2t3(j) = ft3;
+    T1(2,j) = ft1;
+    T2(2,j) = ft2;
+    T3(2,j) = ft3;
 end
 %lambda 3
 for j = 1:length(time)
@@ -69,11 +60,11 @@ for j = 1:length(time)
     %Theta2
     ft2 = f(t,t2,lambda3);
     %Theta3
-    ft3 = f(t,t3,lambda1);
+    ft3 = f(t,t3,lambda3);
     %store into vector
-    f3t1(j) = ft1;
-    f3t2(j) = ft2;
-    f3t3(j) = ft3;
+    T1(3,j) = ft1;
+    T2(3,j) = ft2;
+    T3(3,j) = ft3;
 end
 %lambda 4
 for j = 1:length(time)
@@ -85,154 +76,62 @@ for j = 1:length(time)
     %Theta3
     ft3 = f(t,t3,lambda4);
     %store into vector
-    f4t1(j) = ft1;
-    f4t2(j) = ft2;
-    f4t3(j) = ft3;
+    T1(4,j) = ft1;
+    T2(4,j) = ft2;
+    T3(4,j) = ft3;
 end
 
-%plot 12 graphs for each initial theta and lambda value
-%lambda 1
+%plot graphs for each initial theta and lambda value
+%Theta 1
 figure;
 subplot(2,1,1);
 hold on;
-plot(time, f1t1(1,:));
-
+for j = 1:4
+    plot(time,T1(j,:));
+end
 % Labels and title for the first subplot
-ylabel('Theta1 (deg)');
+ylabel('Theta (deg)');
 xlabel('Time (s)');
-title('Theta 1, Lambda 1 vs. Time');
-legend('0°');
+title(sprintf('Theta vs. Time for Initial Angle %d°', t1));
+legend(sprintf('lambda = %.2f', lambda1), sprintf('lambda = %.2f', lambda2), sprintf('lambda = %.2f', lambda3), sprintf('lambda = %.2f', lambda4));
 
+%Theta 2
 figure;
 subplot(2,1,1);
 hold on;
-plot(time, f1t2(1,:));
-
+for j = 1:4
+    plot(time,T2(j,:));
+end
 % Labels and title for the first subplot
-ylabel('Theta2 (deg)');
+ylabel('Theta (deg)');
 xlabel('Time (s)');
-title('Theta 2, Lambda 1 vs. Time');
-legend('10°');
+title(sprintf('Theta vs. Time for Initial Angle %d°', t2));
+legend(sprintf('lambda = %.2f', lambda1), sprintf('lambda = %.2f', lambda2), sprintf('lambda = %.2f', lambda3), sprintf('lambda = %.2f', lambda4));
 
+%Theta 3
 figure;
 subplot(2,1,1);
 hold on;
-plot(time, f1t3(1,:));
-
+for j = 1:4
+    plot(time,T3(j,:));
+end
 % Labels and title for the first subplot
-ylabel('Theta3 (deg)');
+ylabel('Theta (deg)');
 xlabel('Time (s)');
-title('Theta 3, Lambda 1 vs. Time');
-legend('15°');
+title(sprintf('Theta vs. Time for Initial Angle %d°', t3));
+legend(sprintf('lambda = %.2f', lambda1), sprintf('lambda = %.2f', lambda2), sprintf('lambda = %.2f', lambda3), sprintf('lambda = %.2f', lambda4));
 
-%lambda 2
-figure;
-subplot(2,1,1);
-hold on;
-plot(time, f2t1(1,:));
-
-% Labels and title for the first subplot
-ylabel('Theta1 (deg)');
-xlabel('Time (s)');
-title('Theta 1, Lambda 2 vs. Time');
-legend('0°');
-
-figure;
-subplot(2,1,1);
-hold on;
-plot(time, f2t2(1,:));
-
-% Labels and title for the first subplot
-ylabel('Theta2 (deg)');
-xlabel('Time (s)');
-title('Theta 2, Lambda 2 vs. Time');
-legend('10°');
-
-figure;
-subplot(2,1,1);
-hold on;
-plot(time, f2t3(1,:));
-
-% Labels and title for the first subplot
-ylabel('Theta3 (deg)');
-xlabel('Time (s)');
-title('Theta 3, Lambda 2 vs. Time');
-legend('15°');
-
-%lambda 3
-figure;
-subplot(2,1,1);
-hold on;
-plot(time, f3t1(1,:));
-
-% Labels and title for the first subplot
-ylabel('Theta1 (deg)');
-xlabel('Time (s)');
-title('Theta 1, Lambda 3 vs. Time');
-legend('0°');
-
-figure;
-subplot(2,1,1);
-hold on;
-plot(time, f3t2(1,:));
-
-% Labels and title for the first subplot
-ylabel('Theta2 (deg)');
-xlabel('Time (s)');
-title('Theta 2, Lambda 3 vs. Time');
-legend('0°');
-
-figure;
-subplot(2,1,1);
-hold on;
-plot(time, f3t3(1,:));
-
-% Labels and title for the first subplot
-ylabel('Theta3 (deg)');
-xlabel('Time (s)');
-title('Theta 3, Lambda 3 vs. Time');
-legend('0°');
-
-%lambda 4
-figure;
-subplot(2,1,1);
-hold on;
-plot(time, f4t1(1,:));
-
-% Labels and title for the first subplot
-ylabel('Theta1 (deg)');
-xlabel('Time (s)');
-title('Theta 1, Lambda 4 vs. Time');
-legend('0°');
-
-figure;
-subplot(2,1,1);
-hold on;
-plot(time, f4t2(1,:));
-
-% Labels and title for the first subplot
-ylabel('Theta2 (deg)');
-xlabel('Time (s)');
-title('Theta 2, Lambda 4 vs. Time');
-legend('0°');
-
-figure;
-subplot(2,1,1);
-hold on;
-plot(time, f4t3(1,:));
-
-% Labels and title for the first subplot
-ylabel('Theta3 (deg)');
-xlabel('Time (s)');
-title('Theta 3, Lambda 4 vs. Time');
-legend('0°');
 function y = f(t,theta,lambda)
 m = 0.3;
 g = 9.81;
 r = 1;
 k = sqrt((m*g)/r);
 a = sqrt((k^2/m^2)-(lambda^2/(4*m^2)));
-
-y = theta*exp((-lambda*t)/(2*m))*(cosd(a*t)+(lambda/(2*m*a))*sind(a*t));
+e = exp((-lambda*t)/0.6);
+c = cosd(a*t);
+s1 = lambda/(0.6*a);
+s2 = sind(a*t);
+s = s1*s2;
+y = theta*e*c*s;
 
 end
