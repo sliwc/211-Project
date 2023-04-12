5522f99467f8e678b376b346d506f04d8674c5bd
Sarah Li <sarah_li_7@sfu.ca>
2023-04-11 18:23:50 -0700

    Update A2.m

--- A2.m
+++ A2.m
@@ -19,13 +19,13 @@ ft1 = t1*exp((-lambda2*t)/(2*m))*(cosd(a1*t)+(lambda2/(2*m*a1))*sind(a1*t))
 ft2 = t2*exp((-lambda2*t)/(2*m))*(cosd(a1*t)+(lambda2/(2*m*a1))*sind(a1*t))
 ft3 = t3*exp((-lambda2*t)/(2*m))*(cosd(a1*t)+(lambda2/(2*m*a1))*sind(a1*t))
 
-lambda3 = 0.07;
+lambda3 = 0.07
 a1 = sqrt((k^2/m^2)-(lambda3^2/(4*m^2)));
 ft1 = t1*exp((-lambda3*t)/(2*m))*(cosd(a1*t)+(lambda3/(2*m*a1))*sind(a1*t))
 ft2 = t2*exp((-lambda3*t)/(2*m))*(cosd(a1*t)+(lambda3/(2*m*a1))*sind(a1*t))
 ft3 = t3*exp((-lambda3*t)/(2*m))*(cosd(a1*t)+(lambda3/(2*m*a1))*sind(a1*t))
 
-lambda4 = 0.09;
+lambda4 = 0.09
 a1 = sqrt((k^2/m^2)-(lambda4^2/(4*m^2)));
 ft1 = t1*exp((-lambda4*t)/(2*m))*(cosd(a1*t)+(lambda4/(2*m*a1))*sind(a1*t))
 ft2 = t2*exp((-lambda4*t)/(2*m))*(cosd(a1*t)+(lambda4/(2*m*a1))*sind(a1*t))
