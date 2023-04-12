88eb84e52598f6f63563481599781692602e1016
Kenneth <kenneth.tub@gmail.com>
2023-04-11 21:55:46 -0700

    tabulated A2 results

--- A2.m
+++ A2.m
@@ -9,42 +9,25 @@ t1 = 0;
 t2 = 10;
 t3 = 15;
 
-%evaluate position of the pendulum
-lambda1 = 0.05
-a = sqrt((k^2/m^2)-(lambda^2/(4*m^2)));
-%theta1
-ft1 = f(t1,lambda1)
-%Theta2
-ft2 = f(t2,lambda1)
-%Theta3
-ft3 = f(t3,lambda1)
+%initialize table
+lambda = [0.03; 0.05; 0.07; 0.09];
+theta1 = zeros(size(lambda));
+theta2 = zeros(size(lambda));
+theta3 = zeros(size(lambda));
 
-lambda2 = 0.03
-a = sqrt((k^2/m^2)-(lambda2^2/(4*m^2)));
-%Theta1
-ft1 = f(t1,lambda2)
-%Theta2
-ft2 = f(t2,lambda2)
-%Theta3
-ft3 = f(t3,lambda2)
+%evaluate position of the pendulum for each lambda value
+for i = 1:length(lambda)
+    a = sqrt((k^2/m^2)-(lambda(i)^2/(4*m^2)));
+    %Theta1
+    theta1(i) = f(t1,lambda(i));
+    %Theta2
+    theta2(i) = f(t2,lambda(i));
+    %Theta3
+    theta3(i) = f(t3,lambda(i));
+end
 
-lambda3 = 0.07
-a = sqrt((k^2/m^2)-(lambda3^2/(4*m^2)));
-%Theta1
-ft1 = f(t1,lambda3)
-%Theta2
-ft2 = f(t2,lambda3)
-%Theta1
-ft3 = f(t3,lambda3)
-
-lambda4 = 0.09
-a = sqrt((k^2/m^2)-(lambda4^2/(4*m^2)));
-%Theta1
-ft1 = f(t1,lambda4)
-%Theta2
-ft2 = f(t2,lambda4)
-%Theta3
-ft3 = f(t3,lambda4)
+%create table
+T = table(lambda, theta1, theta2, theta3)
 
 function y = f(theta,lambda)
 m = 0.3;
@@ -54,5 +37,5 @@ r = 1;
 k = sqrt((m*g)/r);
 a = sqrt((k^2/m^2)-(lambda^2/(4*m^2)));
 
-y = theta*exp((-lambda*t)/(2*m))*(cosd(a*t)+(lambda/(2*m*a))*sind(a*t))
+y = theta*exp((-lambda*t)/(2*m))*(cosd(a*t)+(lambda/(2*m*a))*sind(a*t));
 end
